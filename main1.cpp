c24d02fb033fd717369a87c957c4815e0b7284ed
León G <A00573074@itesm.mx>
2022-10-31 01:07:02 +0000

    New

---
+++ main1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void espacio(){
+    cout << endl;
+}
+
+int lasVegas(int limiteInferior, int limiteSuperior) {
+	srand(time(NULL));
+	return (limiteInferior + rand() % (limiteSuperior-limiteInferior+1));
+}
+
+int randomizedBinarySearch(vector<int> datos, int izquierda, int derecha, int busqueda) {
+
+	if (derecha >= izquierda) {
+
+		int medio = lasVegas(izquierda, derecha);
+
+		if (datos[medio] == busqueda){
+			return medio;
+        }
+
+		if (datos[medio] > busqueda){
+		    return randomizedBinarySearch(datos, izquierda, medio-1, busqueda);
+        }
+
+        if (datos[medio] < busqueda){
+		    return randomizedBinarySearch(datos, medio+1, derecha, busqueda);
+        }
+	}
+
+	return -999999999;
+}
+
+int main(void) {
+
+	vector <int> datos;
+	int busqueda;
+    int aux;
+
+    cin >> busqueda;
+
+    cin >> aux;
+    while (aux != -1){
+        datos.push_back(aux);
+        cin >> aux;
+    }
+
+    sort(datos.begin(),datos.end());
+
+	int resultado = randomizedBinarySearch(datos, 0, datos.size()-1, busqueda);
+
+    espacio();
+    if (resultado != -999999999){
+        cout << "El elemento esta presente en el indice: " << resultado << endl;
+    }
+    else {
+        cout << "El elemento no esta presente en el arreglo" << endl;
+    }
+    espacio();
+
+	return 0;
+}
